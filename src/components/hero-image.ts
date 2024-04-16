978a50f27a8771b3d701ab3c4d5aebcdc2d0f066
IvailoStoyanov <ivailo.stoyanov.work@gmail.com>
2024-04-16 18:47:46 +0300

    migrate to vite and lit, add hero-image and lit-nav

---
+++ src/components/hero-image.ts
@@ -0,0 +1,71 @@
+import desktop from "/DesktopHeader.jpg"
+import tablet from "/TabletHeader.jpg";
+import mobile from "/MobileHeader.jpg";
+
+import { LitElement, css, html } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
+
+const mobileMaxWidth = 376;
+const tabletMaxWidth = 769;
+
+const getDefaultHeader = () => {
+  if (window.innerWidth < 376) {
+    return mobile;
+  } else if (window.innerWidth < 769 && window.innerWidth < 376) {
+    return tablet;
+  }
+  return desktop;
+}
+
+@customElement('hero-image')
+export class MyElement extends LitElement {
+  static styles = css`
+    header img {
+      max-width: 100%;
+      height: auto;
+      margin-top: 60px;
+    }
+  `
+  @property({
+    type: {
+      currentHeader: HTMLElement,
+    }
+  })
+  currentHeader = getDefaultHeader();
+
+  private updateHeaderImage() {
+    const headerImage = document.querySelector<HTMLImageElement>("#header-image");
+    if (headerImage) {
+      headerImage.src = this.currentHeader;
+    }
+  }
+
+  private checkScreenSize = () => {
+    if (window.innerWidth < mobileMaxWidth) {
+      this.currentHeader = mobile;
+    } else if (window.innerWidth < tabletMaxWidth && window.innerWidth < mobileMaxWidth) {
+      this.currentHeader = tablet;
+    } else {
+      this.currentHeader = desktop;
+    }
+    this.updateHeaderImage();
+  };
+
+  connectedCallback() {
+    super.connectedCallback();
+    window.addEventListener('resize', this.checkScreenSize);
+    this.updateHeaderImage();
+  }
+  disconnectedCallback() {
+    window.removeEventListener('resize', this.checkScreenSize);
+    super.disconnectedCallback();
+  }
+
+  render() {
+    return html`
+      <header>                 
+        <img id="header-image" src="${this.currentHeader}" alt="Header Image" />
+      </header>
+    `;
+  }
+}
---
+++ src/components/lit-nav.ts
@@ -0,0 +1,136 @@
+import { LitElement, css, html } from 'lit'
+import { customElement, state } from 'lit/decorators.js'
+import hamburger from "/hamburger.svg";
+import cross from "/cross.svg";
+
+const navigationList = [
+  {
+    title: 'начало',
+    url: '#home',
+  },
+  {
+    title: 'хореографи',
+    url: '#choreographers',
+  },
+  {
+    title: 'информация',
+    url: '#info',
+  },
+  {
+    title: 'контакти',
+    url: '#contacts',
+  },
+];
+
+const isMobile = () => {
+  if (window.innerWidth > 769) {
+    console.log('wa');
+    
+    return false;
+  }
+  return true;
+}
+
+@customElement('lit-nav')
+export class Navigation extends LitElement {
+  static styles = css`
+    /* ::slotted(*) { font-family: 'Orbitron'; } */
+
+    .navigationWrapper {
+      background: white;
+      position: fixed;
+      top: 0;
+      width: 100%;
+      z-index: 10;
+    }
+
+    .navigationWrapper.desktop {
+      .hamburger {
+        display: none;
+      }
+
+      .navigation {
+        display: block;
+        padding: 20px;
+      }
+    }
+
+    .navigation {
+      display: none;
+      padding: 10px;
+      flex-direction: column;
+    }
+
+    .hamburger {
+      display: block;
+      margin-left: auto;
+      padding: 10px 20px 5px 10px;
+    }
+
+    a {
+      font-family: 'Orbitron', sans-serif;
+      font-size: 25px;
+      color: #353394;
+      text-decoration: none;
+      padding: 10px;
+    }
+
+    .navigation.visible {
+      display: flex;
+    }
+
+    button {
+      background: none;
+      border: none;
+    }
+
+    /* @media (min-width: 768px) {
+
+    } */
+  `;
+
+  @state() isVisible: boolean = false;
+  @state() isMobileState: boolean = isMobile();
+
+  connectedCallback() {
+    super.connectedCallback();
+    window.addEventListener('resize', () => {
+      this.isMobileState = isMobile();
+    });
+  };
+
+  disconnectedCallback() {
+    window.removeEventListener('resize', () => {
+      this.isMobileState = isMobile();
+    });
+    super.disconnectedCallback();
+  };
+
+  private toggleNavVisibility() {
+    this.isVisible = !this.isVisible
+  };
+
+  private toggleNav() {
+    this.toggleNavVisibility();
+  };
+
+
+  render() {
+    return html`
+    <div class="navigationWrapper ${this.isMobileState ? 'mobile' : 'desktop'}">
+      <button class="hamburger" @click=${this.toggleNav}>
+        <img src=${ !this.isVisible ? hamburger : cross}>
+      </button>
+      <nav class="navigation ${this.isVisible ? 'visible' : null}">
+        ${navigationList.map(({ url, title }) => html`<a href=${url} @click=${this.toggleNav}>${title}</a>`)}
+      </nav>
+    </div>
+    `
+  };
+};
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'lit-nav': Navigation
+  }
+};
